c45cac38b0fd45af5cd65735242b65ef0cc5fdef
Asandke <129843453+Asandke@users.noreply.github.com>
2024-05-19 00:09:04 +0800

    Add files via upload

---
+++ button.js
@@ -0,0 +1,32 @@
+// 获取按钮元素  
+var learnButton1 = document.getElementById('learnButton1');  
+var learnButton2 = document.getElementById('learnButton2');
+var learnButton3= document.getElementById('learnButton3');
+var learnButton5= document.getElementById('learnButton5');
+var knowMoreButton1 = document.getElementById('knowMoreButton1');  
+var knowMoreButton2 = document.getElementById('knowMoreButton2'); 
+
+// 为“马上学习”按钮添加点击事件  
+learnButton1.addEventListener('click', function() {  
+    alert('你已点击“马上学习”按钮，开始你的学习之旅吧！');  
+});
+// 为“马上学习”按钮添加点击事件  
+learnButton2.addEventListener('click', function() {  
+    alert('你已点击“马上学习”按钮，开始你的学习之旅吧！');  
+});
+// 为“马上学习”按钮添加点击事件  
+learnButton3.addEventListener('click', function() {  
+    alert('你已点击“马上学习”按钮，开始你的学习之旅吧！');  
+});  
+  // 为“马上学习”按钮添加点击事件  
+learnButton5.addEventListener('click', function() {  
+    alert('你已点击“马上学习”按钮，开始你的学习之旅吧！');  
+}); 
+// 为“了解”按钮添加点击事件  
+knowMoreButton1.addEventListener('click', function() {  
+    alert('你已点击“了解”按钮，更多信息即将展示。');  
+});
+// 为“了解”按钮添加点击事件  
+knowMoreButton2.addEventListener('click', function() {  
+    alert('你已点击“了解”按钮，更多信息即将展示。');  
+});
